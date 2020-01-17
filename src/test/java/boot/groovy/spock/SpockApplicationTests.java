d53eedee1d86da06a8ade4ee634bff3f7afb32e5
KIM HONGIL <clroyan01@gmail.com>
2020-01-17 18:36:18 +0900

    Create spock project

---
+++ src/test/java/boot/groovy/spock/SpockApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.groovy.spock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
